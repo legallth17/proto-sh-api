76fadd3e4b7d9b530bb30933ee502fdf7433a274
Thierry Le Gall <legallth17@orange.fr>
2015-02-06 23:16:47 +0100

    add authentication

--- server.js
+++ server.js
@@ -2,9 +2,13 @@
 
 var express = require('express');
 var catalog = require('./catalog');
+var expressJwt = require('express-jwt');
+var jwt = require('jsonwebtoken');
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+var secret = 'STY1673HJ19UUY5DFCVFR';
 
 // devices store
 
@@ -18,11 +22,41 @@ var app = express();
 app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded()); // to support URL-encoded bodies
 
+app.use('/devices', expressJwt({secret: secret})); // protect access to /devices
+// allow cors
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
+
 // root
 
 app.get('/', function(req, res) {
         res.send("try POST /devices?type=motion");
 });
+
+app.post('/authenticate', function (req, res) {
+  //TODO validate req.body.username and req.body.password
+  //if is invalid, return 401
+  if (!(req.body.username === 'joe.dalton' && req.body.password === 'alcatraz')) {
+    res.send(401, 'wrong user or password');
+    return;
+  }
+
+  var profile = {
+    first_name: 'Joe',
+    last_name: 'Dalton',
+    email: 'joe.dalton@alcatraz.jail',
+    id: 707
+  };
+
+  // We are sending the profile inside the token
+  var token = jwt.sign(profile, secret, { expiresInMinutes: 30 });
+
+  res.json({ token: token });
+});
+
 
 // POST devices
 
